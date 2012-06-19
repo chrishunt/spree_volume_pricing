fd1f330c38691f3ddabe7e694112b4eaddd0fe46
Adam Anderson <adamandersonis@gmail.com>
2012-06-19 16:20:35 -0400

    Update to use Spree::Order#contains? instead of #find_line_item_by_variant

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,18 +1,34 @@
-Spree::Order.class_eval do
-  # override the add_variant functionality so that we can adjust the price based on possible volume adjustment
-  def add_variant(variant, quantity=1)
-    current_item = find_line_item_by_variant(variant)
-    price = variant.volume_price(quantity) # Added
-    if current_item
-      current_item.quantity += quantity
-      current_item.price = price # Added
-      current_item.save
-    else
-      current_item = line_items.create(:quantity => quantity)
-      current_item.variant = variant
-      current_item.price = price
-      current_item.save
+module Spree
+  Order.class_eval do
+    # override the add_variant functionality so that we can adjust the price based on possible volume adjustment
+    def add_variant(variant, quantity=1)
+      current_item = contains?(variant)
+      price = variant.volume_price(quantity) # Added
+      if current_item
+        current_item.quantity += quantity
+        current_item.price   = price
+        current_item.save
+      else
+        current_item = LineItem.new(:quantity => quantity)
+        current_item.variant = variant
+        current_item.price   = price
+        self.line_items << current_item
+      end
+
+      # populate line_items attributes for additional_fields entries
+      # that have populate => [:line_item]
+      Variant.additional_fields.select { |f| !f[:populate].nil? && f[:populate].include?(:line_item) }.each do |field|
+        value = ''
+
+        if field[:only].nil? || field[:only].include?(:variant)
+          value = variant.send(field[:name].gsub(' ', '_').downcase)
+        elsif field[:only].include?(:product)
+          value = variant.product.send(field[:name].gsub(' ', '_').downcase)
+        end
+        current_item.update_attribute(field[:name].gsub(' ', '_').downcase, value)
+      end
+
+      current_item
     end
-    current_item
   end
 end
